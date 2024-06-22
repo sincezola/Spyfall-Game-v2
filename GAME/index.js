fcf549134b25bdb51cab207ed9ce7824c0548e87
Enzo Zamberlan <enzo2017.bra@gmail.com>
2024-06-22 17:22:39 -0300

    Fiz alguns Error Handlings e criei alguns botões

--- GAME/index.js
+++ GAME/index.js
@@ -1,5 +1,3 @@
-let place = "";
-
 const placesArray = [
   "Hospital",
   "Escola",
@@ -53,12 +51,78 @@ const placesArray = [
   "Cooperativa",
 ]; // The places array
 
+let place = "";
+let isGameRunning = false;
+let playersAmount = 3;
+
+// DOM Navigation -----
+const startButton = document.querySelector("#startButton");
+const endButton = document.querySelector("#endButton");
+const addButton = document.querySelector("#addButton");
+const removeButton = document.querySelector("#removeButton");
+
+addButton.onclick = function () {
+  if (isGameRunning == false) {
+    if (playersAmount < 20) {
+      playersAmount++; // There is a new player in the game
+
+      console.log(`${playersAmount} Players in the game`);
+    } else {
+      error(
+        "The player tried to add more than 20 players, throwing a new error"
+      );
+      alert("Maximum number of players reached");
+    }
+  } else {
+    error(
+      "The player tried to add a player while the game is running, throwing a new error"
+    );
+  }
+};
+
+removeButton.onclick = function () {
+  if (isGameRunning == false) {
+    if (playersAmount > 3) {
+      playersAmount--; // A player have been removed
+
+      console.log(`${playersAmount} Players in the game`);
+    } else {
+      error("The game cannot have less than 3 players, throwing a new error");
+      alert("Minimum number of players reached");
+    }
+  } else {
+    error(
+      "The player tried to remove a player while the game is running, throwing a new error"
+    );
+  }
+};
+
+startButton.onclick = function () {
+  console.log("Game Started");
+
+  isGameRunning = true; // The game started
+};
+
+endButton.onclick = function () {
+  if (isGameRunning == false) {
+    error("O jogo não está em andamento");
+  } else {
+    console.log("Game Ended");
+
+    isGameRunning = false; // The game stopped
+  }
+};
+
 function ChooseAPlace() {
   // When called return a Place
   let randomIndexNumber = Math.floor(Math.random() * 50);
   place = placesArray[randomIndexNumber];
 
   return place;
+}
+
+function error(errorMsg) {
+  console.error(errorMsg);
 }
 
 ChooseAPlace();
